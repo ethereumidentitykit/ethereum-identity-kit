f1c39e49097fb83e02136d723d6516cad50eb9f8
encryptedDegen <encrypted@ethfollow.xyz>
2025-01-09 12:32:48 +0100

    update brand info

--- .storybook/eik-theme.ts
+++ .storybook/eik-theme.ts
@@ -4,7 +4,7 @@ export default create({
   base: 'dark',
   fontBase: 'Inter, sans-serif',
   brandTitle: 'Ethereum Identity Kit',
-  brandUrl: './',
-  brandImage: 'https://ethereumidentitykit.github.io/ethereum-identity-kit/logo-with-text.svg',
+  brandUrl: 'https://ethidentitykit.com',
+  brandImage: 'https://playground.ethidentitykit.com/logo-with-text.svg',
   brandTarget: '_self',
 })
