07f0124e6be99f99ffbd522686e90e9789d74c2d
encryptedDegen <encrypted@ethfollow.xyz>
2025-03-11 21:25:13 +0100

    fix follow button emoji paths

--- src/constants/follow-button.ts
+++ src/constants/follow-button.ts
@@ -17,10 +17,10 @@ export const FOLLOW_BUTTON_STYLES: Record<FollowingState, string> = {
 
 export const FOLLOW_BUTTON_COOL_EMOJI: Record<FollowingState, string | undefined> = {
   Follow: 'https://efp.app/assets/logo.png',
-  Following: 'https://efp.app/assets/icons/unfollow-emoji.svg',
-  Block: 'https://efp.app/assets/icons/block-emoji.svg',
+  Following: 'https://efp.app/assets/icons/emojis/unfollow-emoji.svg',
+  Block: 'https://efp.app/assets/icons/emojis/block-emoji.svg',
   Blocked: undefined,
-  Mute: 'https://efp.app/assets/icons/mute-emoji.svg',
+  Mute: 'https://efp.app/assets/icons/emojis/mute-emoji.svg',
   Muted: undefined,
   'Pending Following': undefined,
   Unfollow: undefined,
